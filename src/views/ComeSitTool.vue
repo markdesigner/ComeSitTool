99ef2b4bb9524ff0a8bd7c96557f09a738bb0ab1
Mark.Chang <mark.chang@vivotek.com>
2025-04-11 13:38:21 +0800

    chore: remove period

--- src/views/ComeSitTool.vue
+++ src/views/ComeSitTool.vue
@@ -149,7 +149,7 @@ export default {
     handleBookingText() {
       this.bookingText = `已經幫您預約囉，那再麻煩三日內幫我匯款${
         this.peopleNumber * 250
-      }元到以下戶頭後，或是Line 加入來坐好友Line Pay(一卡通money)轉帳給我： https://line.me/ti/p/cJ2V_nDAzF。
+      }元到以下戶頭後，或是Line 加入來坐好友Line Pay(一卡通money)轉帳給我： https://line.me/ti/p/cJ2V_nDAzF
 完成後我會傳送入場資訊與密碼給您。非常感謝
 
 銀行代號 007 （第一銀行）
