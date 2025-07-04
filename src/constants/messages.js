7dddf8b4305e4057aff1b66625d4c310c48fd6bb
Mark.Chang <mark.chang@vivotek.com>
2025-07-04 17:17:37 +0800

    fix: update string

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -4,7 +4,7 @@ export const BOOKING_TEXT = ({
 或是Line 加入來坐好友Line Pay(一卡通money)轉帳給我：
 https://line.me/ti/p/cJ2V_nDAzF
 
-完成後我會傳送入場資訊與密碼給您。非常感謝
+完成後我會傳送入場資訊與密碼給您。非常感謝~
 
 銀行代號 007 （第一銀行）
 帳號40157027386
